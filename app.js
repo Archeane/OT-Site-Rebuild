63638cf9e6e326ac6b4c4a46e2de85bd91c194ed
archeane <jenny.xu@stonybrook.edu>
2018-06-27 17:49:09 -0400

    hope i dont regret this

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const errorHandler = require('errorhandler');
+
+//CONTROLLERS (ROUTE HANDLERS)
+const homeController = require('./controllers/home');
+const spotlightController = require('./controllers/spotlight');
+
+
+const app = express();
+app.set('port', process.env.PORT || 3000);
+//Express configuration
+
+//View Engine
+app.engine('pug', require('pug').__express)
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//APP ROUTES
+app.get('/', homeController.index);
+
+/**
+ * Error Handler.
+ */
+
+if (process.env.NODE_ENV === 'development') {
+  // only use in development
+  app.use(errorHandler());
+}
+
+module.exports = app;
